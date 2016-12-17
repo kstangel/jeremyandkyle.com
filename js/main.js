78d166c597885415776d24093a73a6da511a2044
Kyle Stangel <kyle@zebradog.com>
2016-12-17 16:22:36 -0600

    Add Logo

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var isChrome = window.chrome;
+if(isChrome){
+   var bars = document.getElementsByClassName('bar');
+
+   for( var i = 0; i < bars.length; i++){
+      bars[i].style.fontWeight = 'bold';
+   }
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+   var previousDate = moment('2016-02-18');
+   var bars = document.getElementsByClassName('date');
+
+   for (var i = 0; i < bars.length; i++) {
+      var barDate = previousDate.add(1, 'week');
+      var barDateFormat = barDate.format('MMMM Do, YYYY');
+      bars[i].textContent = barDateFormat;
+      previousDate = barDate
+   }
+});
